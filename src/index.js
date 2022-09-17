f7f58d834c1a7c38c06168924081fbe57f236814
rollergod <renat.apaev@yandex.ru>
2022-09-17 18:50:17 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './context/ThemeContext';
@@ -9,6 +8,8 @@ import { CategoryProvider } from './context/CategoryContext';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
