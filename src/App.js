59a6535002d5c3c127bc0a39258d7f8d0563074e
rollergod <renat.apaev@yandex.ru>
2022-09-17 18:38:26 +0300

    test

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <div className={styles.container}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/react-football-players/" element={<Home />} />
+          <Route path="https://rollergod.github.io/react-football-players/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
