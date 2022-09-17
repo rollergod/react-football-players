d2cffd64de720631a9fc4eb6907979321fe49ad9
rollergod <renat.apaev@yandex.ru>
2022-09-17 18:24:30 +0300

    add not found block, router dom and redux

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NotFoundBlock from "../components/NotFoundBlock/NotFoundBlock";
+
+
+const NotFound = () => {
+    return (
+        <NotFoundBlock />
+    )
+}
+
+export default NotFound;
